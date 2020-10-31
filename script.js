594ad3965c38a35aefd3dba3ee157a5334590301
beast-box <larryd.soularian@gmail.com>
2020-10-31 07:23:36 -0400

    adding starting project

---
+++ script.js
@@ -0,0 +1,16 @@
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+
+
+function colorChanger() {
+    body.style.background = "linear-gradient(to right, "
+        + color1.value + ", " + color2.value + ")";
+        css.textContent = body.style.background + ";";
+}
+
+
+color1.addEventListener('input', colorChanger);
+
+color2.addEventListener('input', colorChanger);
